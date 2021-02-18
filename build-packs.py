f09d7c032328ae3e9821e2d5cbb6bc20f3b708d9
mani <manijamali2003@gmail.com>
2021-02-18 19:56:56 +0330

    Update for OS

--- build-packs.py
+++ build-packs.py
@@ -20,62 +20,61 @@ import shutil, os
 
 mode = input('choose your mode (stable,latest): ')
 
-for i in os.listdir('packs'):pack.manifest(i)
-
+for i in os.listdir('packs'):
+    pack.manifest(i)
 shutil.rmtree('stor/tmp')
 shutil.copytree('packs',f'stor/tmp')
 f = open('stor/pack.sa','w')
 f.write(f'''mkdir /tmp/all
+mkdir /tmp/all/{mode}
 paye pak /tmp/baran
-mv /tmp/baran.pa /tmp/all/baran.pa
+mv /tmp/baran.pa /tmp/all/{mode}/baran.pa
 paye pak /tmp/barge
-mv /tmp/barge.pa /tmp/all/barge.pa
+mv /tmp/barge.pa /tmp/all/{mode}/barge.pa
 paye pak /tmp/browser
-mv /tmp/browser.pa /tmp/all/browser.pa
+mv /tmp/browser.pa /tmp/all/{mode}/browser.pa
 paye pak /tmp/calculator
-mv /tmp/calculator.pa /tmp/all/calculator.pa
+mv /tmp/calculator.pa /tmp/all/{mode}/calculator.pa
 paye pak /tmp/calendar
-mv /tmp/calendar.pa /tmp/all/calendar.pa
+mv /tmp/calendar.pa /tmp/all/{mode}/calendar.pa
 paye pak /tmp/commento
-mv /tmp/commento.pa /tmp/all/commento.pa
+mv /tmp/commento.pa /tmp/all/{mode}/commento.pa
 paye pak /tmp/gap
-mv /tmp/gap.pa /tmp/all/gap.pa
+mv /tmp/gap.pa /tmp/all/{mode}/gap.pa
 paye pak /tmp/help
-mv /tmp/help.pa /tmp/all/help.pa
+mv /tmp/help.pa /tmp/all/{mode}/help.pa
 paye pak /tmp/stable
 mv /tmp/stable.pa /tmp/all/stable.pa
 paye pak /tmp/latest
 mv /tmp/latest.pa /tmp/all/latest.pa
 paye pak /tmp/lightword
-mv /tmp/lightword.pa /tmp/all/lightword.pa
+mv /tmp/lightword.pa /tmp/all/{mode}/lightword.pa
 paye pak /tmp/mines
-mv /tmp/mines.pa /tmp/all/mines.pa
+mv /tmp/mines.pa /tmp/all/{mode}/mines.pa
 paye pak /tmp/numix
-mv /tmp/numix.pa /tmp/all/numix.pa
+mv /tmp/numix.pa /tmp/all/{mode}/numix.pa
 paye pak /tmp/paint
-mv /tmp/paint.pa /tmp/all/paint.pa
+mv /tmp/paint.pa /tmp/all/{mode}/paint.pa
 paye pak /tmp/paye
-mv /tmp/paye.pa /tmp/all/paye.pa
+mv /tmp/paye.pa /tmp/all/{mode}/paye.pa
 paye pak /tmp/persia
-mv /tmp/persia.pa /tmp/all/persia.pa
+mv /tmp/persia.pa /tmp/all/{mode}/persia.pa
 paye pak /tmp/pyabr
-mv /tmp/pyabr.pa /tmp/all/pyabr.pa
+mv /tmp/pyabr.pa /tmp/all/{mode}/pyabr.pa
 paye pak /tmp/pyshell
-mv /tmp/pyshell.pa /tmp/all/pyshell.pa
+mv /tmp/pyshell.pa /tmp/all/{mode}/pyshell.pa
 paye pak /tmp/pysys
-mv /tmp/pysys.pa /tmp/all/pysys.pa
+mv /tmp/pysys.pa /tmp/all/{mode}/pysys.pa
 paye pak /tmp/roller
-mv /tmp/roller.pa /tmp/all/roller.pa
+mv /tmp/roller.pa /tmp/all/{mode}/roller.pa
 paye pak /tmp/runapp
-mv /tmp/runapp.pa /tmp/all/runapp.pa
+mv /tmp/runapp.pa /tmp/all/{mode}/runapp.pa
 paye pak /tmp/ubuntu-theme
-mv /tmp/ubuntu-theme.pa /tmp/all/ubuntu-theme.pa
+mv /tmp/ubuntu-theme.pa /tmp/all/{mode}/ubuntu-theme.pa
 paye pak /tmp/windows-theme
-mv /tmp/windows-theme.pa /tmp/all/windows-theme.pa
+mv /tmp/windows-theme.pa /tmp/all/{mode}/windows-theme.pa
 ''')
 f.close()
-shutil.rmtree(mode)
 
 input('Are sure copy all package? press enter')
-
-shutil.copytree('stor/tmp/all',mode)
+shutil.copytree('stor/tmp/all','repo')
